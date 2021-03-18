a33123edb8d74315e541dcb600c019f53ac422e0
Prashant Mital <prashantmital@gmail.com>
2021-03-17 22:33:45 -0700

    automatically attempt to patch everything in `api.__all__`

--- bindings/python/pymongoarrow/monkey.py
+++ bindings/python/pymongoarrow/monkey.py
@@ -11,21 +11,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from pymongoarrow.api import (
-    aggregate_arrow_all, find_arrow_all)
-
-
 __all__ = [
     'patch_all'
 ]
 
 
 def patch_all():
-    for method in (aggregate_arrow_all, find_arrow_all):
+    import pymongoarrow.api as api_module
+    api_methods = api_module.__all__
+    for method_name in api_methods:
+        method = getattr(api_module, method_name)
         if not hasattr(method, "__target__"):
-            raise ValueError(
-                "cannot patch method with unset '__target__' attribute")
-
+            continue
         target_spec = method.__target__
         target_module_name, cls_name = target_spec.rsplit('.', maxsplit=1)
         target_module = __import__(target_module_name, fromlist=[cls_name])
