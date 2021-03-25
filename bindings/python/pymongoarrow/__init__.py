b20b5d47646045fc77721538ad649bf962c9abcf
Shane Harvey <shnhrv@gmail.com>
2021-03-25 14:34:45 -0600

    Fix libbson version ImportError

--- bindings/python/pymongoarrow/__init__.py
+++ bindings/python/pymongoarrow/__init__.py
@@ -27,5 +27,5 @@ except ImportError:
 
 if _parse_version(libbson_version) < _parse_version(_MIN_LIBBSON_VERSION):
     raise ImportError(
-        "Expected libbson version {} or greater, found {}}".format(
-            _MIN_LIBBSON_VERSION, libbson_version))
+        f"Expected libbson version {_MIN_LIBBSON_VERSION} or greater, "
+        f"found {libbson_version}")
