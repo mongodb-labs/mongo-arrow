5619f3ce4ad63d0889deae80ec60600e9e632010
Prashant Mital <5883388+prashantmital@users.noreply.github.com>
2021-04-26 17:31:32 -0700

    BUMP 0.1.0 (#28)

--- bindings/python/pymongoarrow/version.py
+++ bindings/python/pymongoarrow/version.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.1.0.dev0'
+__version__ = '0.1.0'
 
 _MIN_LIBBSON_VERSION = '1.17.0'
