6ff148dc73f7bfb48cc2c6603de53d53b12d9692
Steven Silvester <steven.silvester@ieee.org>
2022-04-07 09:06:17 -0500

    clean up build script

--- bindings/python/cibw_before_build.sh
+++ bindings/python/cibw_before_build.sh
@@ -3,15 +3,33 @@
 set -o xtrace
 set -o errexit
 
-# Handle architectures from cibuildwheel
-CMAKE_OSX_ARCHITECTURES="x86_64"
-if [[ "$ARCHFLAGS" == *"arm64"* ]]
+# Handle architectures from cibuildwheel.
+# Set CMAKE_OSX_ARCHITECTURES for libbson.
+# Get the appropriate platoform for pip to download pyarrow.
+
+mac_version="${MACOSX_DEPLOYMENT_TARGET/\./_}"
+
+if [[ "$CIBW_BUILD" == *"macosx_"* ]]
 then
-  # Compile libbson with arm64 support
-  CMAKE_OSX_ARCHITECTURES="arm64;x86_64"
-  # Download pyarrow with arm64 support
-  pip install --platform macosx_11_0_arm64 --no-deps --only-binary=:all: pyarrow
+  os_name=macosx
+else if [[ "$CIBW_BUILD" == *"win_amd64"* ]]
+then
+  os_name=win_amd64
+else
+  os_name=manylinux_x86_64
 fi
 
+if [[ "$ARCHFLAGS" == *"arm64"* ]]
+then
+  CMAKE_OSX_ARCHITECTURES="arm64;x86_64"
+  platform="${os_name}_${mac_version}_universal2"
+else
+  CMAKE_OSX_ARCHITECTURES="x86_64"
+  platform="${os_name}_${mac_version}_x86_64"
+fi
 
+# Build libbson with the appropriate arch.
 ./build_libbson
+
+# Install pyarrow with the appropriate platform.
+pip install --platform $platform --no-deps --only-binary=:all: pyarrow
