11a1f175a5fb65b4464103526c230b34c904fe41
Steven Silvester <steven.silvester@ieee.org>
2022-08-05 13:33:36 -0500

    ARROW-98 Test Against Capped Collections

--- bindings/python/test/test_pymongoarrow.py
+++ bindings/python/test/test_pymongoarrow.py
@@ -11,12 +11,35 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from unittest import TestCase
+import unittest
+from test import client_context
 
+from pymongoarrow.api import find_arrow_all
+from pymongoarrow.schema import Schema
 from pymongoarrow.version import __version__
 
 
-class TestPyMongoArrow(TestCase):
+class TestPyMongoArrow(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        if not client_context.connected:
+            raise unittest.SkipTest("cannot connect to MongoDB")
+        cls.client = client_context.get_client()
+
     def test_version(self):
         self.assertIsNotNone(__version__)
         self.assertIsInstance(__version__, str)
+
+    def test_capped_collection(self):
+        self.client.test.drop_collection("test")
+        self.client.test.create_collection("test", capped=True, size=5000)
+        schema = Schema({"data": bool})
+        data = [{"data": False} for _ in range(1000)]
+        self.client.test.test.insert_many(data)
+
+        # Data should have been capped.
+        cursor = self.client.test.test.find({})
+        total = len(list(cursor))
+        self.assertLess(total, 1000)
+        table = find_arrow_all(self.client.test.test, {}, schema=schema)
+        self.assertEqual(table.shape, (total, 1))
