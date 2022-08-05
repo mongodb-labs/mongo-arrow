96477bc93427f9d584f3a92f49c026c4976121d3
Steven Silvester <steven.silvester@ieee.org>
2022-08-05 15:23:43 -0500

    ARROW-106 Support PyArrow 8.0 (#86)

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -158,11 +158,15 @@ def append_arrow_flags(module):
         pass
 
     # Find the appropriate library file and optionally copy it locally.
-    for name in pa.get_libraries():
+    # Explicitly handle "parquet" library as a workaround for
+    # https://issues.apache.org/jira/browse/ARROW-17327
+    for name in pa.get_libraries() + ["parquet"]:
         if IS_WIN:
             if COPY_LIBARROW:
                 lib_file = os.path.join(arrow_lib, f"{name}.dll")
                 if not os.path.exists(lib_file):
+                    if name == "parquet":
+                        continue
                     raise ValueError("Could not find compiled arrow library")
                 shutil.copy(lib_file, BUILD_DIR)
             lib_file = os.path.join(arrow_lib, f"{name}.lib")
