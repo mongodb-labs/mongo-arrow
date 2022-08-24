aa36474a378fda4c9a4bfaeb853b92a6de758bc7
Steven Silvester <steven.silvester@ieee.org>
2022-08-24 08:59:39 -0500

    fix benchmark and install

--- bindings/python/benchmark.py
+++ bindings/python/benchmark.py
@@ -46,58 +46,77 @@ def _setup():
     small = db[collection_names[SMALL]]
     small.drop()
 
-    print(
-        "%d small docs, %d bytes each with 3 keys"
-        % (N_SMALL_DOCS, len(BSON.encode({"_id": ObjectId(), "x": 1, "y": math.pi})))
-    )
+    if SMALL in sizes:
+        print(
+            "%d small docs, %d bytes each with 3 keys"
+            % (N_SMALL_DOCS, len(BSON.encode({"_id": ObjectId(), "x": 1, "y": math.pi})))
+        )
 
-    small.insert_many(
-        [collections.OrderedDict([("x", 1), ("y", math.pi)]) for _ in range(N_SMALL_DOCS)]
-    )
+        small.insert_many(
+            [collections.OrderedDict([("x", 1), ("y", math.pi)]) for _ in range(N_SMALL_DOCS)]
+        )
 
-    dtypes[SMALL] = np.dtype([("x", np.int64), ("y", np.float64)])
-    schemas[SMALL] = Schema({"x": pyarrow.int64(), "y": pyarrow.float64()})
+        dtypes[SMALL] = np.dtype([("x", np.int64), ("y", np.float64)])
+        schemas[SMALL] = Schema({"x": pyarrow.int64(), "y": pyarrow.float64()})
 
-    large = db[collection_names[LARGE]]
-    large.drop()
-    # 2600 keys: 'a', 'aa', 'aaa', .., 'zz..z'
-    large_doc_keys = [c * i for c in string.ascii_lowercase for i in range(1, 101)]
-    large_doc = collections.OrderedDict([(k, math.pi) for k in large_doc_keys])
-    print(
-        "%d large docs, %dk each with %d keys"
-        % (N_LARGE_DOCS, len(BSON.encode(large_doc)) // 1024, len(large_doc_keys))
-    )
+        # Ignore for now that the first batch defaults to 101 documents.
+        raw_bson_docs_small = [{"x": 1, "y": math.pi} for _ in range(N_SMALL_DOCS)]
+        raw_bson_small = BSON.encode(
+            {
+                "ok": 1,
+                "cursor": {
+                    "id": Int64(1234),
+                    "ns": "db.collection",
+                    "firstBatch": raw_bson_docs_small,
+                },
+            }
+        )
 
-    large.insert_many([large_doc.copy() for _ in range(N_LARGE_DOCS)])
+    if LARGE in sizes:
+        large = db[collection_names[LARGE]]
+        large.drop()
+        # 2600 keys: 'a', 'aa', 'aaa', .., 'zz..z'
+        large_doc_keys = [c * i for c in string.ascii_lowercase for i in range(1, 101)]
+        large_doc = collections.OrderedDict([(k, math.pi) for k in large_doc_keys])
+        print(
+            "%d large docs, %dk each with %d keys"
+            % (N_LARGE_DOCS, len(BSON.encode(large_doc)) // 1024, len(large_doc_keys))
+        )
 
-    dtypes[LARGE] = np.dtype([(k, np.float64) for k in large_doc_keys])
-    schemas[LARGE] = Schema({k: pyarrow.float64() for k in large_doc_keys})
+        large.insert_many([large_doc.copy() for _ in range(N_LARGE_DOCS)])
 
-    # Ignore for now that the first batch defaults to 101 documents.
-    raw_bson_docs_small = [{"x": 1, "y": math.pi} for _ in range(N_SMALL_DOCS)]
-    raw_bson_small = BSON.encode(
-        {
-            "ok": 1,
-            "cursor": {"id": Int64(1234), "ns": "db.collection", "firstBatch": raw_bson_docs_small},
-        }
-    )
+        dtypes[LARGE] = np.dtype([(k, np.float64) for k in large_doc_keys])
+        schemas[LARGE] = Schema({k: pyarrow.float64() for k in large_doc_keys})
 
-    raw_bson_docs_large = [large_doc.copy() for _ in range(N_LARGE_DOCS)]
-    raw_bson_large = BSON.encode(
-        {
-            "ok": 1,
-            "cursor": {"id": Int64(1234), "ns": "db.collection", "firstBatch": raw_bson_docs_large},
-        }
-    )
+        raw_bson_docs_large = [large_doc.copy() for _ in range(N_LARGE_DOCS)]
+        raw_bson_large = BSON.encode(
+            {
+                "ok": 1,
+                "cursor": {
+                    "id": Int64(1234),
+                    "ns": "db.collection",
+                    "firstBatch": raw_bson_docs_large,
+                },
+            }
+        )
 
-    raw_bsons[SMALL] = raw_bson_small
-    raw_bsons[LARGE] = raw_bson_large
-    arrow_tables[SMALL] = find_arrow_all(db[collection_names[SMALL]], {}, schema=schemas[SMALL])
-    arrow_tables[LARGE] = find_arrow_all(db[collection_names[LARGE]], {}, schema=schemas[LARGE])
-    pandas_tables[SMALL] = find_pandas_all(db[collection_names[SMALL]], {}, schema=schemas[SMALL])
-    pandas_tables[LARGE] = find_pandas_all(db[collection_names[LARGE]], {}, schema=schemas[LARGE])
-    numpy_arrays[SMALL] = find_numpy_all(db[collection_names[SMALL]], {}, schema=schemas[SMALL])
-    numpy_arrays[LARGE] = find_numpy_all(db[collection_names[LARGE]], {}, schema=schemas[LARGE])
+    if SMALL in sizes:
+        raw_bsons[SMALL] = raw_bson_small
+        arrow_tables[SMALL] = find_arrow_all(db[collection_names[SMALL]], {}, schema=schemas[SMALL])
+        assert arrow_tables[SMALL].shape == (100000, 2)
+        pandas_tables[SMALL] = find_pandas_all(
+            db[collection_names[SMALL]], {}, schema=schemas[SMALL]
+        )
+        numpy_arrays[SMALL] = find_numpy_all(db[collection_names[SMALL]], {}, schema=schemas[SMALL])
+
+    if LARGE in sizes:
+        raw_bsons[LARGE] = raw_bson_large
+        arrow_tables[LARGE] = find_arrow_all(db[collection_names[LARGE]], {}, schema=schemas[LARGE])
+        assert arrow_tables[LARGE].shape == (1000, 2600)
+        pandas_tables[LARGE] = find_pandas_all(
+            db[collection_names[LARGE]], {}, schema=schemas[LARGE]
+        )
+        numpy_arrays[LARGE] = find_numpy_all(db[collection_names[LARGE]], {}, schema=schemas[LARGE])
 
 
 def _teardown():
