2b5781972505ec29530ee865456f65c746948af5
Steven Silvester <steven.silvester@ieee.org>
2022-08-26 11:30:37 -0500

    try again

--- bindings/python/get_ld_path.sh
+++ bindings/python/get_ld_path.sh
@@ -6,4 +6,5 @@ set -o errexit
 pip install pyarrow
 ARROW_LIB=$(python -c "import pyarrow;print(':'.join(pyarrow.get_library_dirs()))")
 ls -ltr $ARROW_LIB
-export REPAIR_LIBRARY_PATH="$ARROW_LIB:$LD_LIBRARY_PATH"
+LIBBSON=$(pwd)/libbson/lib64
+export REPAIR_LIBRARY_PATH="$ARROW_LIB:$LIBBSON:$LD_LIBRARY_PATH"
