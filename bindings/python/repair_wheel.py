bf23d621b99fee8bc83d4048f6a20a89161aa0e8
Steven Silvester <steven.silvester@ieee.org>
2022-08-30 17:21:42 -0500

    address review

--- bindings/python/repair_wheel.py
+++ bindings/python/repair_wheel.py
@@ -1,6 +1,8 @@
+import atexit
 import glob
 import os
 import sys
+import tempdir
 from subprocess import run
 
 HERE = os.path.abspath(os.path.dirname(__file__))
@@ -12,7 +14,8 @@ wheel_file = wheel_file.replace(os.sep, "/")
 if "universal2" in wheel_file:
     macos_ver = os.environ.get("MACOSX_DEPLOYMENT_TARGET", "10.3")
     macos_ver = macos_ver.replace(".", "_")
-    target = os.path.expanduser("~/wheels")
+    wheel_dir = tempdir.TemporaryDirectory()
+    atexit.register(wheel_dir.cleanup)
     run(
         [
             sys.executable,
@@ -23,13 +26,13 @@ if "universal2" in wheel_file:
             f"macosx_{macos_ver}_universal2",
             "--upgrade",
             "--target",
-            target,
+            wheel_dir.name,
             "--only-binary=:all:",
             "pyarrow",
         ]
     )
     # Allow the installed pyarrow library to be imported.
-    sys.path.insert(0, target)
+    sys.path.insert(0, wheel_dir.name)
 else:
     run([sys.executable, "-m", "pip", "install", "pyarrow"])
 import pyarrow as pa  # noqa
@@ -44,7 +47,7 @@ extra_paths = pa.get_library_dirs() + libbson_lib
 extra_path = os.path.pathsep.join([a.replace(os.sep, "/") for a in extra_paths])
 
 
-def append_os_variable(name):
+def append_os_variable(name, extra_path):
     if os.environ.get(name):
         os.environ[name] = os.environ[name] + os.pathsep + extra_path
     else:
@@ -53,7 +56,7 @@ def append_os_variable(name):
 
 
 if os.name == "nt":
-    append_os_variable("PATH")
+    append_os_variable("PATH", extra_path)
     run([sys.executable, "-m", "pip", "install", "delvewheel"])
     run(["delvewheel", "repair", "--no-mangle", "ucrtbased.dll", "-w", wheel_dir, wheel_file])
 
@@ -62,7 +65,7 @@ elif sys.platform == "darwin":
     site_pkgs = sys.base_prefix
     dylib = glob.glob(f"{sys.base_prefix}/lib/python*/lib-dynload")[0]
     extra_path = f"{dylib}:{extra_path}"
-    append_os_variable("DYLD_LIBRARY_PATH")
+    append_os_variable("DYLD_LIBRARY_PATH", extra_path)
     run([sys.executable, "-m", "pip", "install", "delocate"])
     run(
         [
@@ -75,6 +78,6 @@ elif sys.platform == "darwin":
         ]
     )
 else:
-    append_os_variable("LD_LIBRARY_PATH")
+    append_os_variable("LD_LIBRARY_PATH", extra_path)
     run([sys.executable, "-m", "pip", "install", "auditwheel"])
     run(["auditwheel", "repair", "-w", wheel_dir, wheel_file])
