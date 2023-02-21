722df95df79f4dad6f99d63e90ab41fdf1087cfc
Julius Park <Juliusgeo@gmail.com>
2023-02-21 14:28:52 -0800

    ARROW-150 Improve documentation about limitations surrounding schemas and nested data structures (#130)

--- bindings/python/pymongoarrow/schema.py
+++ bindings/python/pymongoarrow/schema.py
@@ -27,6 +27,8 @@ class Schema:
     Each key in ``schema`` is a field name and its corresponding value
     is the expected type of the data contained in the named field.
 
+    For more examples, see :ref:`schema usage`.
+
     Data types can be specified as pyarrow type instances (e.g.
     an instance of :class:`pyarrow.int64`), bson types (e.g.
     :class:`bson.Int64`), or python type-identifiers (e.g. ``int``,
