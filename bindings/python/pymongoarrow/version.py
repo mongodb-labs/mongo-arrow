3b8c388e5cd4b3498b619ffe9037e88db8991c0a
Steven Silvester <steven.silvester@ieee.org>
2023-06-22 13:42:05 -0500

    BUMP 1.1.0.dev0

--- bindings/python/pymongoarrow/version.py
+++ bindings/python/pymongoarrow/version.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1.0.1"
+__version__ = "1.1.0.dev0"
 
 _MIN_LIBBSON_VERSION = "1.21.0"
