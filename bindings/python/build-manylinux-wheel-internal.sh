cddb1afd1e936d11f7870b0a04b068a6020bf6d4
Steven Silvester <steven.silvester@ieee.org>
2023-09-08 21:10:40 -0500

    fix handling of env variable

--- bindings/python/build-manylinux-wheel-internal.sh
+++ bindings/python/build-manylinux-wheel-internal.sh
@@ -6,6 +6,6 @@ rm -rf $LIBBSON_INSTALL_DIR
 rm -rf build dist
 PYTHON=/opt/python/cp311-cp311/bin/python
 $PYTHON -m pip install build
-$PYTHON -m build --wheel .
+LIBBSON_INSTALL_DIR=$LIBBSON_INSTALL_DIR $PYTHON -m build --wheel .
 rm -rf $LIBBSON_INSTALL_DIR
 rm -rf build
