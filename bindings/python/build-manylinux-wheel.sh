24250d1c3add2d73ca37afb59487056322279a42
Steven Silvester <steven.silvester@ieee.org>
2023-09-11 12:33:39 -0500

    revert wheel building on linux

--- bindings/python/build-manylinux-wheel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ex
-
-set -o xtrace
-set -o errexit
-
-image="quay.io/pypa/manylinux2014_x86_64"
-docker pull $image
-docker run --rm -v "`pwd`:/src" $image /src/build-manylinux-wheel-internal.sh
