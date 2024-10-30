5d093756ac26d787c181ba72d8f03fa4be0f7bec
Steven Silvester <steve.silvester@mongodb.com>
2024-10-30 06:37:47 -0500

    update macos

--- bindings/python/cibw_before_build.sh
+++ bindings/python/cibw_before_build.sh
@@ -11,7 +11,7 @@ if [[ "$CIBW_BUILD" == *"macosx_"* ]]
 then
   if [[ "$ARCHFLAGS" == *"arm64"* ]]
   then
-    platform="macosx_11_0_arm64"
+    platform="macosx_12_0_arm64"
     export CMAKE_OSX_ARCHITECTURES="arm64"
   else
     platform="macosx_12_0_x86_64"
