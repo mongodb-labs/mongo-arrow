a7d05c61a391753cf6b5e0359af56e63fc09ce09
Steven Silvester <steve.silvester@mongodb.com>
2025-04-22 08:41:13 -0500

    clean up conftest

--- bindings/python/test/__init__.py
+++ bindings/python/test/__init__.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import atexit
 
 import pymongo
 
@@ -41,8 +40,6 @@ class ClientContext:
 
         if self.connected:
             self.client = self.get_client()
-            atexit.register(self.client.close)
 
 
 client_context = ClientContext()
-client_context.init()
