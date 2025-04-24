94798d4ec7a8d5da598daf098ca8886a4471d53d
Steven Silvester <steve.silvester@mongodb.com>
2025-04-24 13:02:51 -0500

    INTPYTHON-592 Fix version bump script for release process

--- .github/scripts/bump-version.sh
+++ .github/scripts/bump-version.sh
@@ -2,4 +2,4 @@
 set -eu
 
 CURRENT_VERSION=$(NO_EXT=1 python setup.py --version)
-sed -i '' "s/__version__ = \"${CURRENT_VERSION}\"/__version__ = \"$1\"/" "pymongoarrow/version.py"
+sed -i "s/__version__ = \"${CURRENT_VERSION}\"/__version__ = \"$1\"/" "pymongoarrow/version.py"
