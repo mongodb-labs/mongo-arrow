a481736e013ddc83203df6159c42056f97ec5cec
Steven Silvester <steven.silvester@ieee.org>
2025-04-24 11:38:00 -0500

    INPYTHON-592 Fix Release Version Bump Handling (#301)

---
+++ .github/scripts/bump-version.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -eu
+
+CURRENT_VERSION=$(NO_EXT=1 python setup.py --version)
+sed -i '' "s/__version__ = \"${CURRENT_VERSION}\"/__version__ = \"$1\"/" "pymongoarrow/version.py"
