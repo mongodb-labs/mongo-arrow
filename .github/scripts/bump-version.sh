9686e2730e2fabbeec3d4be83295fcebc361e851
Steven Silvester <steve.silvester@mongodb.com>
2025-04-24 10:08:30 -0500

    remove debug

--- .github/scripts/bump-version.sh
+++ .github/scripts/bump-version.sh
@@ -2,5 +2,4 @@
 set -eu
 
 CURRENT_VERSION=$(NO_EXT=1 python setup.py --version)
-echo "HELLO $CURRENT_VERSION"
 sed -i '' "s/__version__ = \"${CURRENT_VERSION}\"/__version__ = \"$1\"/" "pymongoarrow/version.py"
